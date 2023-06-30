14fc8c4cacd7b1351705a2e2e8316ed2aa466396
monisha <82220436+MONISHA895@users.noreply.github.com>
2023-07-01 00:47:43 +0530

    Add files via upload

---
+++ src/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NewsList from './components/NewsList';
+
+function App() {
+  return (
+    <div className="App">
+    <NewsList/>
+ </div>
+  );
+}
+
+export default App;
